372c425788f7f447bc7c52651bc1440cf5e84e0d
MerlinSMQWQ <MerlinSMQWQ@proton.me>
2025-09-26 00:20:25 +0800

    docs(src/test_system.py): 添加注释

--- src/test_system.py
+++ src/test_system.py
@@ -3,6 +3,7 @@ import Product
 import Order
 from Order import OrderStatus
 
+# 使用pytest测试Product、User、Order是否实现
 def test_order_with_available_stock():
     product = Product(product_id='product-001', stock = 10, price = 20)
     user = User(user_id='user-001', balance = 1)
