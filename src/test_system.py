5ac53e6a3ae2cba060fa37a232fef11677f0c25a
MerlinSMQWQ <MerlinSMQWQ@proton.me>
2025-09-26 00:17:28 +0800

    test(src/test_system.py): 新建一个测试，用于驱动用户、商品、订单开发

---
+++ src/test_system.py
@@ -0,0 +1,12 @@
+import User
+import Product
+import Order
+from Order import OrderStatus
+
+def test_order_with_available_stock():
+    product = Product(product_id='product-001', stock = 10, price = 20)
+    user = User(user_id='user-001', balance = 1)
+    order = Order(order_id = 'order-001', user = user, product = product, quantity = 5)
+
+    assert product.stock == 5
+    assert order.status == OrderStatus.UNPAID
